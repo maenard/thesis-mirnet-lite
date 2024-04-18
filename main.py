d77a3516f25af5c7a0f0719db3e64cc518d4e51f
maenard <maenardaboabo065@gmail.com>
2024-04-18 23:38:55 +0800

    refactored mirnet

--- main.py
+++ main.py
@@ -3,13 +3,10 @@ from PIL import Image
 from matplotlib import pyplot as plt
 from MIRNet.mirnet.inference import Inferer
 from MIRNet.mirnet.utils import download_dataset, plot_result
+import os
 
 import tensorflow as tf
 import numpy as np
-
-#download_dataset for testing (run only once)
-#download_dataset('LOL')
-
 LOW_LIGHT_IMGS = glob('./eval15/low/*')
 
 MODEL_DICT = {
@@ -18,22 +15,13 @@ MODEL_DICT = {
     "int8": "mirnet_int8.tflite"
 }
 
-def preprocess_image(image_path, image_resize_factor=1):
+def infer_tflite(model_type, image_path):
     original_image = Image.open(image_path)
-    width, height = original_image.size
-    preprocessed_image = original_image.resize(
-        (
-            width // image_resize_factor,
-            height // image_resize_factor
-        ),
-        Image.ANTIALIAS)
-    preprocessed_image = tf.keras.preprocessing.image.img_to_array(preprocessed_image)
-    preprocessed_image = preprocessed_image.astype('float32') / 255.0
-    preprocessed_image = np.expand_dims(preprocessed_image, axis=0)
-    
-    return original_image, preprocessed_image
+    image = original_image.resize((input_width, input_height), Image.ANTIALIAS)
+    image = tf.keras.preprocessing.image.img_to_array(image)
+    image = image.astype('float32') / 255.0
+    image = np.expand_dims(image, axis=0)
 
-def infer_tflite(model_type, image):
     interpreter = tf.lite.Interpreter(model_path=MODEL_DICT[model_type])
     input_index = interpreter.get_input_details()[0]["index"]
     output_index = interpreter.get_output_details()[0]["index"]
@@ -42,20 +30,26 @@ def infer_tflite(model_type, image):
     interpreter.allocate_tensors()
     interpreter.set_tensor(input_index, image)
     interpreter.invoke()
-    raw_prediction = interpreter.tensor(output_index)
-    output_image = raw_prediction()
+    raw_prediction = interpreter.tensor(output_index)()
+    
+    output_image = raw_prediction.squeeze() * 255.0
+    output_image = np.clip(output_image, 0, 255).astype(np.uint8)
+    output_image = Image.fromarray(output_image)
+    
+    return original_image, output_image
 
-    output_image = output_image.squeeze() * 255.0
-    output_image = output_image.clip(0, 255)
-    output_image = output_image.reshape(
-        (np.shape(output_image)[0], np.shape(output_image)[1], 3)
-    )
-    output_image = Image.fromarray(np.uint8(output_image))
-    return output_image
+# Create a directory to save the output images
+output_dir = "output_images"
+os.makedirs(output_dir, exist_ok=True)
 
-model_type = "fp16" #@param ["dr", "fp16", "int8"]
+model_type = "int8" #dr, fp16, int8
+input_width, input_height = 64, 64
 
-for image_path in LOW_LIGHT_IMGS[:5]:
-    original_image, preprocessed_image = preprocess_image(image_path)
-    output_image = infer_tflite(model_type, preprocessed_image)
-    plot_result(Image.fromarray(np.uint8(original_image)), output_image)
+for i, image_path in enumerate(LOW_LIGHT_IMGS[:5]):
+    original_image, output_image = infer_tflite(model_type, image_path)
+    
+    # Save the output image to the output directory
+    output_path = os.path.join(output_dir, f"output_{i}.jpg")
+    output_image.save(output_path)
+
+print("Output images saved in the 'output_images' directory.")
